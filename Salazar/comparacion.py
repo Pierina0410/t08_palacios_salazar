cab13536ab36fcf82dfc7d49e006605cff430c45
Pierina0410 <57008210+pierina0410@users.noreply.github.com>
2019-12-13 13:54:53 -0500

    comparacion de mi compañero

---
+++ Salazar/comparacion.py
@@ -0,0 +1,53 @@
+#ejercicio1-se hara la comparación de las palabras Conferencia y chocolatada
+print("chocolatada"== "chocolatada")
+
+#ejercicio2-se hara la comparación de las frases Corazon y higado
+print("corazon"!="higado")
+
+#ejercicio3-se hara la comparación de las paabras FEMINISMO y MACHISMO
+print("feminismo"<"Machismo")
+
+
+#ejercicio4-se hara la comparación de las frases "Solo sé que nada sé " y "el fin justifica los medios"
+print("Solo sé que nada sé " <"el fin justifica los medios")
+
+##ejercicio5-se hara la comparación de las frases "Nicolas Maquiavelo" y "Edgar Allan Poe"
+print("Nicolas Maquiavelo"=="Edgar Allan Poe" )
+
+
+
+#ejercicio6-se hara la comparación de los nombres "alicia" y "Ariana"
+print("alicia" < "Ariana")
+
+
+#ejercicio7-se hara la comparación de las frases "Tengo una serpiente" y "bota"
+print("Tengo una serpiente" < "bota" )
+
+
+#ejercicio8-se hara la comparación de las frases "chifa" y "tottus"
+print("chifa" != "tottus")
+
+#ejercici9-se hara la comparación de las frases "musica criolla" y "salsa"
+print(  "musica criolla" == "salsa ")
+
+##ejercici10 se hara la comparación de las frases "PROGRAMACION" y "programacion"
+print("PROGRAMACION">"programacion")
+
+##ejercici11 se hara la comparación de las frases "MARIA" Y "maria"
+print("MARIA" <= "maria")
+
+##ejercici12 se hara la comparación de las frases "Oxido" "acido"
+print( "Oxido"  != "acido")
+
+
+##ejercici113 se hara la comparación de las frases "libertad" "prision"
+print("libertad"== "prision")
+
+
+##ejercicio14 se hara la comparación de las frases "amor" y "amor"
+print("amor" == "amor")
+
+##ejercicio15 se hara la comparación de las frases "hola mundo" y "anderson"
+print("hola mundo" != "anderson")
+
+
