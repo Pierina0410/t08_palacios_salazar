babff7047bcd900a7dfabf0981a32c569cf0cd95
Pierina0410 <57008210+pierina0410@users.noreply.github.com>
2019-12-13 14:41:16 -0500

    se agrego la busqueda  de mi compañero

---
+++ Salazar/busqueda.py
@@ -0,0 +1,32 @@
+str="El mundo es un absurdo animado que rueda en el vacío para asombro de sus habitantes"
+
+  #EJERCICIO1
+print(str.find("El"))
+#EJERCICIO2
+print(str.index("es"))
+#EJERCICIO3
+print(str.find("un"))
+#Ejercicio4
+print(str.index("El es un"))
+#Ejercicio5
+print(str.find("absurdo"))
+#ejercicio6
+print(str.index("vacio"))
+#Ejercicio7
+print(str.index("animado es el"))
+#ejercicio8
+print(str.index("asombro"))
+#ejercicio9
+print(str.find("habitantes"))
+#ejercicio10
+print(str.index("anda en el vacio"))
+#ejercicio11
+print(str.find("mundo"))
+#ejercicio12
+print(str.index("El es mudo"))
+#ejercicio13
+print(str.find("un mundo"))
+#ejercicio14
+print(str.index("vacio"))
+#ejercicio15
+print(str.find("El es absurdo"))
