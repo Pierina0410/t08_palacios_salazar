5df09f80ecf2ac83788449a34bc3617c067adf1c
Pierina0410 <57008210+pierina0410@users.noreply.github.com>
2019-12-13 14:28:43 -0500

    se agrego la busqueda y formato de mi compañero

---
+++ Salazar/formato.py
@@ -0,0 +1,77 @@
+#Ejercicio1
+msg="Hey{} tu promedio es {}"
+print(msg.format("Smith","09"))
+
+#Ejercicio2
+msg="Carlos {}  muchos {}"
+print(msg.format("tiene","perros"))
+
+#Ejercicio3
+msg="Hoy {}  pensaba {}"
+print(msg.format("aún","en ti "))
+
+#Ejercicio4
+msg="promedio"
+print(msg.rjust(08))
+
+
+#Ejercicio4
+msg="cantidad de ribonucleico"
+print(msg.rjust(20))
+
+
+#Ejercicio5
+msg="Mirely"
+print(msg.rjust(330))
+
+#ejercicio6
+portentaje="56"
+print(portentaje.zfill(10))
+
+#ejercicio7
+probabilidad="123"
+print(probabilidad.zfill(4))
+#ejercicio8
+msg="Computadora{0:>30}"
+print(msg.format("monitor"))
+
+#ejercicio9
+msg="Ropa{8:>34}"
+print(msg.format("verduraa"))
+
+#ejercicio10
+msg="mi conejo come {:d} verduras"
+print(msg.format(32))
+
+#ejercicio11
+msg="Mi examen duro {:d}  minutos"
+print(msg.format(56))
+
+#ejercicio11
+msg="llego en  {:d} horas"
+print(msg.format(3))
+
+#ejercicio12
+x=345
+msg="El valor de x es {} "
+print(msg.format(x))
+
+#ejercicio12
+X=345
+msg="El valor de X es {} "
+print(msg.format(X))
+
+#EJERCICIO13
+#Ejercicio1
+msg="Roxana {} de nota  {}"
+print(msg.format("tiene","20"))
+
+#ejercicio14
+
+msg="Tengo {:d} clases en la tarde"
+print(msg.format(2))
+
+#ejercicio15
+msg="precio de {producto}"
+item="pollo"
+print(msg.format(producto=item))
