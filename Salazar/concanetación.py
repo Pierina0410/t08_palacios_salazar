44cba2610c305267b83d2f7172efe3b5bbd294b7
Pierina0410 <57008210+pierina0410@users.noreply.github.com>
2019-12-13 13:55:17 -0500

    concanetacion de mi compañero

---
+++ Salazar/concanetación.py
@@ -0,0 +1,48 @@
+#EJERCICIO1
+print("toda"+"loca")
+
+#EJERCICIO2
+print("peli"+"\r"+"rojo")
+
+#Ejercicio3
+print("aereo"+"puerto")
+
+#Ejercicio4
+print("gran"+"corazon")
+
+#Ejercicio5
+print("saca"+"corcho")
+
+#ejercicio6
+print("toma"+"todo")
+
+#ejercicio7
+print("limpia"+"botas")
+
+#ejercico8
+print("mucho"+"amor")
+
+#ejercicio9
+print("puerto"+"cholo")
+
+#ejercicio10
+print("lapiz"+"rojo")
+
+#ejercico11
+print("big" + "mother")
+
+#ejercicio12
+nombre="rosa" + "maria"
+print("hola"+"\n" + nombre)
+
+
+#ejercicio13
+nombre="Maria" + "jose"
+print("DondeEstas" + nombre)
+
+#Ejercicio14
+print("piña"+"colada")
+
+#ejercicio15
+print("todo"+"jugador")
+
